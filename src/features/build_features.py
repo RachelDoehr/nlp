7ec3154ccbd61e02d9a4027033ebe97d35763c31
RachelDoehr <33264020+RachelDoehr@users.noreply.github.com>
2020-08-07 13:11:53 -0400

    nlp preprocessing

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -8,9 +8,23 @@ import json
 import datetime
 import gzip
 import shutil
+import re
+import os
 import io
 import pandas as pd
 import numpy as np
+
+from sqlalchemy import create_engine
+
+import preprocessor as p
+from nltk.corpus import stopwords
+import spacy
+import nltk
+from nltk.tokenize import TweetTokenizer
+import string
+from nltk import wordpunct_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 
 BUCKET = 'tweets-1301' # s3 bucket name
 
@@ -21,15 +35,29 @@ class TrainingDataMaker():
     and if the author intends to receive a flu vaccine. Analysis of this dataset was published in:
 
     Xiaolei Huang, Michael C. Smith, Michael Paul, Dmytro Ryzhkov, Sandra Quinn, David Broniatowski, Mark Dredze. Examining Patterns of Influenza Vaccination in Social Media. 
-    AAAI Joint Workshop on Health Intelligence (W3PHIAI), 2017.'''
+    AAAI Joint Workshop on Health Intelligence (W3PHIAI), 2017.
+    
+    Performs preprocessing.'''
     
     def __init__(self, logger):
 
         self.logger = logger
         self.s3_client = boto3.client('s3')
         self.raw_data_path = Path(__file__).resolve().parents[2].joinpath('data').resolve().joinpath('raw').resolve()
+        self.interim_data_path = Path(__file__).resolve().parents[2].joinpath('data').resolve().joinpath('interim').resolve()
+        self.processed_data_path = Path(__file__).resolve().parents[2].joinpath('data').resolve().joinpath('processed').resolve()
 
-    def get_data(self):
+        oov_df = pd.read_csv(self.raw_data_path.joinpath('OOV_dict.csv')) # load the out of vocabulary file
+        self.oov_informal = oov_df.informal.to_list()
+        self.oov_formal = oov_df.translated.to_list()
+
+        self.nlp = spacy.load("en_core_web_sm")
+        self.ps = PorterStemmer()
+
+        # tweet preprocessing setup ----------------------------------------
+        p.set_options(p.OPT.URL, p.OPT.EMOJI, p.OPT.MENTION, p.OPT.RESERVED, p.OPT.SMILEY) # get rid of everything but hashtags and numbers
+
+    def get_training_data(self):
 
         '''Reads in gzip JSON from s3, saves JSON locally.'''
         obj = self.s3_client.get_object(Bucket=BUCKET, Key='flu_vaccine.json_.gz')
@@ -41,8 +69,23 @@ class TrainingDataMaker():
                 shutil.copyfileobj(f_in, f_out)
                 f_out.close()
 
-        self.logger.info('loaded data, now cleaning...')
-    
+        self.logger.info('loaded training data, now cleaning...')
+
+    def get_covid19_vac_tweets(self):
+
+        '''Queries the PostresSQL DB on AWS to get the COVID-19 incoming tweets.
+        The DB is constantly streaming them in so for testing purposes here have put a limit of 5,000 for speed.
+        
+        [Considering productionizing this part later if I make a dashboard]'''
+
+        db_string = "postgresql+psycopg2://postgres:" + POSTGRES_PASSWORD + "@database.cgiiehbwlxvq.us-east-2.rds.amazonaws.com"
+        engine = create_engine(db_string)
+
+        self.raw_cv19_df = pd.read_sql(
+            "SELECT * FROM twitter.streamed_vaccine LIMIT 1000",
+            con=engine
+        )
+
     def clean_data(self):
 
         '''Turns JSON into prepped dataframe with needed info'''
@@ -56,7 +99,6 @@ class TrainingDataMaker():
             'tweet_id': [],
             'tweet_text': [],
             'is_retweet': [],
-            'is_truncated': [],
             'FLU_vac_intend_to_receive': [],
             'FLU_vac_received': [],
             'FLU_vac_relevant': [],
@@ -89,12 +131,112 @@ class TrainingDataMaker():
                 D['FLU_vac_sentiment'].append('NA')
         
         self.raw_df = pd.DataFrame.from_dict(D, orient='index').transpose()
-        print(self.raw_df.head())
+        self.raw_df.to_csv(self.interim_data_path.joinpath('raw_df.csv').resolve())
+        self.logger.info('handled JSON -> CSV conversion, saved locally...')
+
+    def preprocess(self, t, lowercase=True):
+
+        '''Initial removal of strange Tweet characters'''
+
+        processed_tweet = p.clean(t)
+
+        # Remove all the special characters
+        processed_tweet = re.sub(r'\W', ' ', processed_tweet)
+
+        # remove all single characters
+        processed_tweet = re.sub(r'\s+[a-zA-Z]\s+', ' ', processed_tweet)
+    
+        # Remove single characters from the start
+        processed_tweet = re.sub(r'\^[a-zA-Z]\s+', ' ', processed_tweet) 
+    
+        # Substituting multiple spaces with single space
+        processed_tweet= re.sub(r'\s+', ' ', processed_tweet, flags=re.I)
+    
+        # Removing prefixed 'b'
+        processed_tweet = re.sub(r'^b\s+', '', processed_tweet)
+
+        if lowercase:
+            processed_tweet = processed_tweet.lower()
+        return processed_tweet
+
+    def token_process(self, raw_text):
+
+        """
+        Takes in a string of text, then performs the following:
+        remove all punctuation, remove all stopwords, returns a list of the cleaned text
+        """
+
+        # Check characters to see if they are in punctuation
+        nopunc = [char for char in list(raw_text) if char not in string.punctuation]
+        # Join the characters again to form the string
+        nopunc = ''.join(nopunc)
+        # Now just remove any stopwords
+        return [word for word in nopunc.lower().split() if word.lower() not in stopwords.words('english')]
+    
+    def replace(self, token):
+
+        '''Helper function for removing out of vocabulary words'''
+
+        replacement = None
+        if token in self.oov_informal:
+            replacement = self.oov_formal[self.oov_informal.index(token)]
+        else:
+            replacement = token
+        return replacement
+    
+    def replace_OOV_words(self, text_token_list):
+
+        '''
+        Replace the informal tweet words like B2B, etc. with the human-annotated ones that are most likely to be correct.
+        Documentation and OOV dictionary available in https://crisisnlp.qcri.org/lrec2016/lrec2016.html
+        '''
+    
+        text_token_list_formal = [self.replace(t) for t in text_token_list]
+        return text_token_list_formal
+    
+    def _gen_lemmas(self, tweet_col):
+        # Parse the sentence using the loaded 'en' model object `nlp` using spacy
+        sentence = ' '.join([c for c in tweet_col])
+        doc = self.nlp(sentence)
+        # Extract the lemma for each token and join
+        return [token.lemma_ for token in doc]
+    
+    def _gen_stems(self, tweet_col):
+        return [self.ps.stem(w) for w in tweet_col]
+
+    # ------------------------------------- end Tweet processing setup
+
+    def preprocess_tweet_df(self, df, tweet_text_col, lemma=False):
+        
+        '''Applies the tweet preprocessing functions in order'''
+
+        out_df = df.copy()
+
+        self.logger.info('preprocessing: removing emojis, strange characters...')
+        out_df[tweet_text_col] = out_df[tweet_text_col].apply(str)
+        out_df['tweet_clean'] = out_df.apply(lambda L: self.preprocess(L[tweet_text_col]), axis=1)
+        self.logger.info('preprocessing: turning into tokens...')
+        out_df['tweet_tokens'] = out_df.apply(lambda L: self.token_process(L.tweet_clean), axis=1)
+        self.logger.info('preprocessing: translating out-of-vocabulary words...')
+        out_df['tweet_tokens_formal'] = out_df.apply(lambda L: self.replace_OOV_words(L.tweet_tokens), axis=1)
+        self.logger.info('preprocessing: generating stems...')
+        out_df['tweet_stems_formal'] = out_df.apply(lambda L: self._gen_stems(L.tweet_tokens), axis=1)
+
+        if lemma:
+            self.logger.info('preprocessing: generating lemmas (this takes time)...')
+            out_df['tweet_lemmas_formal'] = out_df.apply(lambda L: self._gen_lemmas(L.tweet_tokens_formal), axis=1)
+
+        return out_df
 
     def execute_dataprep(self):
 
-        #self.get_data()
+        #self.get_training_data()
+        self.get_covid19_vac_tweets()
         self.clean_data()
+        #self.training_clean_df = self.preprocess_tweet_df(self.raw_df, 'tweet_text', lemma=True)
+        #self.training_clean_df.to_csv(self.processed_data_path.joinpath('training_nlp.csv'))
+        self.cv19_clean_df = self.preprocess_tweet_df(self.raw_cv19_df, 'tweet_text', lemma=True)
+        self.cv19_clean_df.to_csv(self.processed_data_path.joinpath('cv19_nlp.csv'))
 
 
 def main():
@@ -113,5 +255,7 @@ if __name__ == '__main__':
 
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
+    
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
     main()
